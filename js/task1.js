8017fe751d9b18128bdf340e524f5072dd9ce8e1
Roman13456 <romankhromishin@gmail.com>
2022-06-29 10:44:43 +0300

    add dots and responsive

--- js/task1.js
+++ js/task1.js
@@ -1,70 +1,118 @@
 // Пишемо свій слайдер зображень
-
 // відображаємо зображення та кнопки Next, Prev з боків від зображення
 // При натисканні на Next - показуємо наступне зображення
 // При натисканні на Prev - попереднє
 // При досягненні останнього зображення – ховати кнопку Next. Аналогічно з першим зображенням та кнопкою
 const imgArray = document.querySelectorAll(".slider img")
+const imgContainersArray = document.querySelectorAll(".slider .imgContainer div")
+const sliderUi = document.querySelector(".sliderUi")
 const slider = document.querySelector(".slider")
 const prevBtn = document.querySelector(".prevBtn")
 const nextBtn = document.querySelector(".nextBtn")
-const sliderParent = slider.parentElement
-const imgArrayLength = imgArray.length
-console.dir(sliderParent)
+const sliderDotsContainer = document.querySelector(".sliderDots")
 const imgContainer = document.querySelector(".imgContainer")
-const sliderParentWidth = sliderParent.offsetWidth
-const sliderParentHeight = sliderParent.offsetHeight
+const sliderParent = slider.parentElement.parentElement
+const imgArrayLength = imgArray.length
+// let sliderParentWidth = slider.parentElement.parentElement.offsetWidth
+let sliderParentWidthWithoutButtons = ""
+let sliderParentHeight = sliderParent.offsetHeight
+// console.dir(prevBtn)
 let shift = 0
 let counter = 0
-let imgContainerStyle = ""
-slider.style=`max-width:${sliderParentWidth}px`
-window.onload = imgArray.forEach(function(element){
-    element.style=`max-width:${sliderParentWidth}px; height:${sliderParentHeight}px`
-    const imgContainerWidth = sliderParentWidth*imgArrayLength
-    imgContainerStyle+=`width: ${imgContainerWidth}px;`
-    imgContainer.style=imgContainerStyle
-    console.log(shift)
-})
+// let imgContainerStyle = ""
+function centerImg(array){
+    // console.dir(img)
+    // console.dir(img.offsetWidth)
+    array.forEach(function(element){
+        let leftAndRightMargins = (sliderParentWidthWithoutButtons - element.offsetWidth)/2
+        let topAndBottomMargins = (sliderParentHeight - element.offsetHeight)/2
+        element.style.margin=`${topAndBottomMargins}px ${leftAndRightMargins}px`
+    })
+}
+addDots()
+function sliderInit(){
+    activeDot()
+    btnLimits()
+    sliderParentWidthWithoutButtons = sliderParent.offsetWidth-prevBtn.clientWidth-nextBtn.clientWidth
+    sliderUi.style.width = `${sliderParent.offsetWidth}px`
+    sliderDotsContainer.style.maxWidth=`${sliderParent.offsetWidth*0.5}px`
+    sliderParentHeight = sliderParent.offsetHeight
+    slider.style=`max-width:${sliderParentWidthWithoutButtons}px`
+    const imgContainerWidth = sliderParentWidthWithoutButtons*imgArrayLength
+    imgContainer.style.width=`${imgContainerWidth}px`
+    imgContainersArray.forEach(function(element){
+        element.style=`width:${sliderParentWidthWithoutButtons}px; `
+    })
+    imgArray.forEach(function(element){
+        element.style=`max-width:${sliderParentWidthWithoutButtons}px; max-height:${sliderParentHeight}px`
+    })
+    centerImg(document.querySelectorAll(".slider img"))
+}
+window.onload = sliderInit
+window.addEventListener("resize",sliderInit)
+window.addEventListener("resize",disableTransform)
 function btnLimits(){
     if(counter==imgArrayLength-1){
         nextBtn.setAttribute("disabled","")
+        nextBtn.classList.add("disabled")
     }else{
         nextBtn.removeAttribute("disabled")
+        nextBtn.classList.remove("disabled")
     }
     if(counter==0){
         prevBtn.setAttribute("disabled","")
+        prevBtn.classList.add("disabled")
     }else{
         prevBtn.removeAttribute("disabled")
+        prevBtn.classList.remove("disabled")
     }
 }
 function addTransform(){
-    imgContainerStyle+= `transform:translate(${shift}px);`
-    imgContainer.style=imgContainerStyle
+    imgContainer.style.transform=`translate(${shift}px)`
+    let styleValue = imgContainer.getAttribute("style")
+    let place = styleValue.search("transition")
+    styleValue=styleValue.slice(0,place)
+    imgContainer.style=styleValue
 }
-// nextBtn.addEventListener("click", function(){
-    // imgArray[counter].classList.remove("visible")
-    // counter+=1
-    // imgArray[counter].classList.add("visible")
-    // btnLimits()
-// })
-// prevBtn.addEventListener("click", function(){
-//     imgArray[counter].classList.remove("visible")
-//     counter-=1
-//     imgArray[counter].classList.add("visible")
-//     btnLimits()
-// })
-// imgContainer.style = imgContainerStyle
+function disableTransform(){
+    imgContainer.style.transform="none"
+    imgContainer.style.transition="none"
+    shift = 0
+    counter=0
+}
 nextBtn.addEventListener("click",function(){
-    shift-=sliderParentWidth;
-    console.log(shift)
+    shift-=sliderParentWidthWithoutButtons;
     addTransform()
     counter+=1
+    activeDot()
     btnLimits()
 })
 prevBtn.addEventListener("click",function(){
-    shift+=sliderParentWidth;
-    console.log(shift)
+    shift+=sliderParentWidthWithoutButtons;
     addTransform()
     counter-=1
+    activeDot()
     btnLimits()
 })
+function activeDot(){
+    const sliderDotsContainerArray =  sliderDotsContainer.querySelectorAll("i")
+    sliderDotsContainerArray.forEach(function(element){
+        element.classList.remove("active")
+    })
+    sliderDotsContainerArray[counter].classList.add("active")
+}
+function addDots(){
+    for(let i=0; i<imgContainersArray.length; i++){
+        sliderDotsContainer.insertAdjacentHTML("beforeend",'<i></i>')
+        const sliderDotsArray = sliderDotsContainer.querySelectorAll("i")
+        sliderDotsArray[i].addEventListener("click",function(){
+            counter=i
+            shift = -(i*sliderParentWidthWithoutButtons)
+            activeDot()
+            addTransform()
+            btnLimits()
+        })
+        
+    }
+}
+
